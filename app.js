aea842f7597df0f7b76df35f145b5e86147dee2e
catherine <catherine@mesasix.com>
2016-07-15 21:23:45 +0800

    added blocks end point

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ app.get('/', function (request, response) {
    response.send('Hello There!');
 });
 
+app.get('/blocks', function (request, response){
+   var blocks = ['Fixed', 'Movable', 'Rotating'];
+    response.json(blocks); //same as .send()
+});
+
 app.listen(8080, function () {
     console.log("Listening to port 8080");
 });
